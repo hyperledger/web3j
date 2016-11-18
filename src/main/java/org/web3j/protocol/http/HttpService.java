7702d1ded0313fb854cfdbd4a26a31943f79970b
Conor <conor10@gmail.com>
2016-11-18 22:02:25 +1100

    1. Added support for specifying Ethereum client versions when working with Infura. 2. Updated network diagram.

--- src/main/java/org/web3j/protocol/http/HttpService.java
+++ src/main/java/org/web3j/protocol/http/HttpService.java
@@ -2,9 +2,12 @@ package org.web3j.protocol.http;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
@@ -12,6 +15,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 
 import org.web3j.protocol.Web3jService;
 import org.web3j.protocol.ObjectMapperFactory;
@@ -25,23 +29,26 @@ public class HttpService implements Web3jService {
 
     public static final String DEFAULT_URL = "http://localhost:8545/";
 
-    private CloseableHttpClient httpClient =
-            HttpClients.custom().setConnectionManagerShared(true).build();
+    private CloseableHttpClient httpClient;
 
-    private ObjectMapper objectMapper = ObjectMapperFactory.getObjectMapper();
+    private final ObjectMapper objectMapper = ObjectMapperFactory.getObjectMapper();
 
     private final String url;
 
-    public HttpService() {
-        this.url = DEFAULT_URL;
+    public HttpService(String url, CloseableHttpClient httpClient) {
+        this.url = url;
+        this.httpClient = httpClient;
     }
 
     public HttpService(String url) {
-        this.url = url;
+        this(url, HttpClients.custom().setConnectionManagerShared(true).build());
     }
 
-    public HttpService(String url, CloseableHttpClient httpClient) {
-        this.url = url;
+    public HttpService() {
+        this(DEFAULT_URL);
+    }
+
+    protected void setHttpClient(CloseableHttpClient httpClient) {
         this.httpClient = httpClient;
     }
 
@@ -53,7 +60,8 @@ public class HttpService implements Web3jService {
 
         HttpPost httpPost = new HttpPost(this.url);
         httpPost.setEntity(new ByteArrayEntity(payload));
-        httpPost.setHeader("Content-Type", "application/json; charset=UTF-8");
+        Header[] headers = buildHeaders();
+        httpPost.setHeaders(headers);
 
         ResponseHandler<T> responseHandler = getResponseHandler(responseType);
         try {
@@ -62,6 +70,15 @@ public class HttpService implements Web3jService {
             httpClient.close();
         }
     }
+
+    private Header[] buildHeaders() {
+        List<Header> headers = new ArrayList<>();
+        headers.add(new BasicHeader("Content-Type", "application/json; charset=UTF-8"));
+        addHeaders(headers);
+        return headers.toArray(new Header[0]);
+    }
+
+    protected void addHeaders(List<Header> headers) { }
 
     public <T> ResponseHandler<T> getResponseHandler(Class<T> type) {
         return response -> {
---
+++ src/test/java/org/web3j/protocol/infura/InfuraHttpServiceTest.java
@@ -0,0 +1,29 @@
+package org.web3j.protocol.infura;
+
+
+import org.apache.http.message.BasicHeader;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.web3j.protocol.infura.InfuraHttpService.buildHeader;
+
+public class InfuraHttpServiceTest {
+
+    @Test
+    public void testBuildHeader() {
+        assertNull(buildHeader("", false));
+        assertNull(buildHeader(null, false));
+
+        assertThat(buildHeader("geth 1.4.19", true).toString(),
+                is(new BasicHeader(
+                        "Infura-Ethereum-Preferred-Client",
+                        "geth 1.4.19").toString()));
+
+        assertThat(buildHeader("geth 1.4.19", false).toString(),
+                is(new BasicHeader(
+                        "Infura-Ethereum-Preferred-Client",
+                        "geth 1.4.19; required=false").toString()));
+    }
+}
