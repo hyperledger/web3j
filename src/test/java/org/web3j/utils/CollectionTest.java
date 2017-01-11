cf22800bfe7ea2a71ad8b5e9b19f107f839cd877
fooock <nhitbh@gmail.com>
2017-01-11 23:05:22 +0100

    Remove blank spaces before and after of each element of the list passed to the join method from the Collection class

--- src/test/java/org/web3j/utils/CollectionTest.java
+++ src/test/java/org/web3j/utils/CollectionTest.java
@@ -3,6 +3,9 @@ package org.web3j.utils;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.web3j.utils.Collection.*;
@@ -22,5 +25,52 @@ public class CollectionTest {
         assertThat(create("a"), is(new String[] { "a" }));
         assertThat(create(""), is(new String[] { "" }));
         assertThat(create("a", "b"), is(new String[] { "a", "b" }));
+    }
+
+    @Test
+    public void testJoin() {
+        assertThat(join(Arrays.asList("a  ", "b ", " c "), ","), is("a,b,c"));
+        assertThat(join(Arrays.asList("a", "b", "c", "d"), ","), is("a,b,c,d"));
+        assertThat(join(Arrays.asList("a  ", "b ", " c "), ", "), is("a, b, c"));
+        assertThat(join(Arrays.asList("a", "b", "c", "d"), ", "), is("a, b, c, d"));
+
+        final List<FakeSpec> specs1 = Arrays.asList(
+                new FakeSpec("a"),
+                new FakeSpec("b"),
+                new FakeSpec("c"));
+        assertThat(join(specs1, ",", FakeSpec::getName), is("a,b,c"));
+
+        final List<FakeSpec> specs2 = Arrays.asList(
+                new FakeSpec("a"),
+                new FakeSpec("b"),
+                new FakeSpec("c"));
+        assertThat(join(specs2, ", ", FakeSpec::getName), is("a, b, c"));
+
+        final List<FakeSpec> specs3 = Arrays.asList(
+                new FakeSpec(" a"),
+                new FakeSpec("b  "),
+                new FakeSpec(" c "));
+        assertThat(join(specs3, ",", FakeSpec::getName), is("a,b,c"));
+
+        final List<FakeSpec> specs4 = Arrays.asList(
+                new FakeSpec(" a"),
+                new FakeSpec("b  "),
+                new FakeSpec(" c "));
+        assertThat(join(specs4, ", ", FakeSpec::getName), is("a, b, c"));
+    }
+
+    /**
+     * Fake object to test {@link Collection#join(List, String, Function)}
+     */
+    private final class FakeSpec {
+        private final String name;
+
+        private FakeSpec(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
