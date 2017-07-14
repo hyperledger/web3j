e0b89f750c520bf2a0e901af4b3c4798a4c99b7a
Assaf <assafyossifoff@sky.com>
2017-07-14 15:53:36 +0100

    reverted Hash.java back, changed isAddress to use .getBytes() instead

--- src/main/java/org/web3j/utils/Account.java
+++ src/main/java/org/web3j/utils/Account.java
@@ -1,11 +1,11 @@
 package org.web3j.utils;
 
-import org.web3j.crypto.Hash;
-
 import java.util.regex.Pattern;
 
+import org.web3j.crypto.Hash;
+
 /**
- * Account utility functions
+ * Account utility functions.
  */
 public class Account {
 
@@ -14,7 +14,7 @@ public class Account {
     private static final Pattern upperCaseAddrPattern = Pattern.compile("^(0x)?[0-9A-F]{40}$");
 
     /**
-     * Verify that a hex account string is a valid Ethereum address
+     * Verify that a hex account string is a valid Ethereum address.
      *
      * @param address given address in HEX
      * @return is this a valid address
@@ -24,30 +24,27 @@ public class Account {
          * check basic address requirements, i.e. is not empty and contains
          * the valid number and type of characters
          */
-        if (address.isEmpty() || !ignoreCaseAddrPattern.matcher(address).find())
+        if (address.isEmpty() || !ignoreCaseAddrPattern.matcher(address).find()) {
             return false;
-        /*
-         * if it's all small caps or caps return true
-         */
-        else if (lowerCaseAddrPattern.matcher(address).find() ||
-                upperCaseAddrPattern.matcher(address).find())
+        } else if (lowerCaseAddrPattern.matcher(address).find()
+                || upperCaseAddrPattern.matcher(address).find()) {
+            // if it's all small caps or caps return true
             return true;
-        /*
-         * if it is mixed caps it is a checksum address and needs to be validated
-         */
-        else
+        } else {
+            // if it is mixed caps it is a checksum address and needs to be validated
             return validateChecksumAddress(address);
+        }
     }
 
     private static Boolean validateChecksumAddress(String address) {
-        String hash = Hash.sha3(address).replace("0x", "");
+        String hash = Numeric.toHexString(Hash.sha3(address.replace("0x", "").getBytes()));
         for (int i = 0; i < 40; i++) {
             if (Character.isLetter(address.charAt(i))) {
                 // each uppercase letter should correlate with a first bit of 1 in the hash
                 // char with the same index, and each lowercase letter with a 0 bit
                 int charInt = Integer.parseInt(Character.toString(hash.charAt(i)), 16);
-                if ((Character.isUpperCase(address.charAt(i)) && charInt <= 7) ||
-                        (Character.isLowerCase(address.charAt(i)) && charInt > 7)) {
+                if ((Character.isUpperCase(address.charAt(i)) && charInt <= 7)
+                        || (Character.isLowerCase(address.charAt(i)) && charInt > 7)) {
                     return false;
                 }
             }
