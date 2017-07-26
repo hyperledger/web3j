6844e817c75bd72de9e731233ecd63e173dfbd0d
Assaf <assafyossifoff@sky.com>
2017-07-26 08:48:14 +0100

    changed toHexString() to toHexStringNoPrefix()

--- src/main/java/org/web3j/utils/Account.java
+++ src/main/java/org/web3j/utils/Account.java
@@ -38,7 +38,7 @@ public class Account {
 
     private static Boolean validateChecksumAddress(String address) {
         String formattedAddress = address.replace("0x", "").toLowerCase();
-        String hash = Numeric.toHexString(Hash.sha3(formattedAddress.getBytes()));
+        String hash = Numeric.toHexStringNoPrefix(Hash.sha3(formattedAddress.getBytes()));
         for (int i = 0; i < 40; i++) {
             if (Character.isLetter(address.charAt(i))) {
                 // each uppercase letter should correlate with a first bit of 1 in the hash
