2fdab586c7c0dd58e6cb5b62d986580442d18f23
Ivaylo Kirilov <iikirilov@gmail.com>
2017-08-05 10:35:09 +0100

    override equals and hashcode

--- src/main/java/org/web3j/crypto/WalletFile.java
+++ src/main/java/org/web3j/crypto/WalletFile.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.Objects;
 
 /**
  * Ethereum wallet file.
@@ -61,6 +62,43 @@ public class WalletFile {
     public void setVersion(int version) {
         this.version = version;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.address);
+        hash = 37 * hash + Objects.hashCode(this.crypto);
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + this.version;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WalletFile other = (WalletFile) obj;
+        if (this.version != other.version) {
+            return false;
+        }
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.crypto, other.crypto)) {
+            return false;
+        }
+        return true;
+    }   
 
     public static class Crypto {
         private String cipher;
@@ -134,6 +172,52 @@ public class WalletFile {
         public void setMac(String mac) {
             this.mac = mac;
         }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 17 * hash + Objects.hashCode(this.cipher);
+            hash = 17 * hash + Objects.hashCode(this.ciphertext);
+            hash = 17 * hash + Objects.hashCode(this.cipherparams);
+            hash = 17 * hash + Objects.hashCode(this.kdf);
+            hash = 17 * hash + Objects.hashCode(this.kdfparams);
+            hash = 17 * hash + Objects.hashCode(this.mac);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Crypto other = (Crypto) obj;
+            if (!Objects.equals(this.cipher, other.cipher)) {
+                return false;
+            }
+            if (!Objects.equals(this.ciphertext, other.ciphertext)) {
+                return false;
+            }
+            if (!Objects.equals(this.kdf, other.kdf)) {
+                return false;
+            }
+            if (!Objects.equals(this.mac, other.mac)) {
+                return false;
+            }
+            if (!Objects.equals(this.cipherparams, other.cipherparams)) {
+                return false;
+            }
+            if (!Objects.equals(this.kdfparams, other.kdfparams)) {
+                return false;
+            }
+            return true;
+        }       
+        
     }
 
     public static class CipherParams {
@@ -149,6 +233,32 @@ public class WalletFile {
         public void setIv(String iv) {
             this.iv = iv;
         }
+
+        @Override
+        public int hashCode() {
+            int hash = 3;
+            hash = 89 * hash + Objects.hashCode(this.iv);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final CipherParams other = (CipherParams) obj;
+            if (!Objects.equals(this.iv, other.iv)) {
+                return false;
+            }
+            return true;
+        }        
+        
     }
 
     interface KdfParams {
@@ -197,6 +307,43 @@ public class WalletFile {
         public void setSalt(String salt) {
             this.salt = salt;
         }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 53 * hash + this.dklen;
+            hash = 53 * hash + this.c;
+            hash = 53 * hash + Objects.hashCode(this.prf);
+            hash = 53 * hash + Objects.hashCode(this.salt);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Aes128CtrKdfParams other = (Aes128CtrKdfParams) obj;
+            if (this.dklen != other.dklen) {
+                return false;
+            }
+            if (this.c != other.c) {
+                return false;
+            }
+            if (!Objects.equals(this.prf, other.prf)) {
+                return false;
+            }
+            if (!Objects.equals(this.salt, other.salt)) {
+                return false;
+            }
+            return true;
+        }        
     }
 
     public static class ScryptKdfParams implements KdfParams {
@@ -248,6 +395,47 @@ public class WalletFile {
         public void setSalt(String salt) {
             this.salt = salt;
         }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 97 * hash + this.dklen;
+            hash = 97 * hash + this.n;
+            hash = 97 * hash + this.p;
+            hash = 97 * hash + this.r;
+            hash = 97 * hash + Objects.hashCode(this.salt);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final ScryptKdfParams other = (ScryptKdfParams) obj;
+            if (this.dklen != other.dklen) {
+                return false;
+            }
+            if (this.n != other.n) {
+                return false;
+            }
+            if (this.p != other.p) {
+                return false;
+            }
+            if (this.r != other.r) {
+                return false;
+            }
+            if (!Objects.equals(this.salt, other.salt)) {
+                return false;
+            }
+            return true;
+        }       
     }
 
     // If we need to work with MyEtherWallet we'll need to use this deserializer, see the
