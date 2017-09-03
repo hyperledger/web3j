4289552042e098e55cb82947d653ad045662298a
dmi3coder <dmi3coder@gmail.com>
2017-09-03 13:33:52 +0300

    fixed bouncycastle imports to spongycastle

--- src/main/java/org/web3j/crypto/Wallet.java
+++ src/main/java/org/web3j/crypto/Wallet.java
@@ -15,10 +15,10 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.bouncycastle.crypto.digests.SHA256Digest;
-import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
-import org.bouncycastle.crypto.generators.SCrypt;
-import org.bouncycastle.crypto.params.KeyParameter;
+import org.spongycastle.crypto.digests.SHA256Digest;
+import org.spongycastle.crypto.generators.PKCS5S2ParametersGenerator;
+import org.spongycastle.crypto.generators.SCrypt;
+import org.spongycastle.crypto.params.KeyParameter;
 
 import org.web3j.utils.Numeric;
 
