4230ee51dfb1540317cda41f8f45fdfcb07df303
Assaf <assafyossifoff@sky.com>
2017-09-14 10:29:39 +0100

    fixed style fails

--- src/test/java/org/web3j/utils/AccountTest.java
+++ src/test/java/org/web3j/utils/AccountTest.java
@@ -9,10 +9,10 @@ import static org.web3j.utils.Account.isAddress;
 
 public class AccountTest {
 
-    private final static String validLowerCaseAddress = "0x3de8c14c8e7a956f5cc4d82beff749ee65fdc358";
-    private final static String validChecksumAddress = "0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359";
-    private final static String invalidLengthAddress = "0x3de8c14c8e7a956f5cc4d82beff749ee65bac35";
-    private final static String invalidChecksumAddress = "0x3de8c14c8E7a956f5cc4d82beff749ee65fdc358";
+    private final String validLowerCaseAddress = "0x3de8c14c8e7a956f5cc4d82beff749ee65fdc358";
+    private final String validChecksumAddress = "0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359";
+    private final String invalidLengthAddress = "0x3de8c14c8e7a956f5cc4d82beff749ee65bac35";
+    private final String invalidChecksumAddress = "0x3de8c14c8E7a956f5cc4d82beff749ee65fdc358";
 
     @Test
     public void verifyIsAddress() {
