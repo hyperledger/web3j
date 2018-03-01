040831c066ed55722b1217ed68515bfc9930e36f
xavi <xavi.arias.segui@gmail.com>
2018-03-01 20:43:14 +0000

    Update Smart Contract wrappers' ABIs, binaries and generated Java files

--- integration-tests/src/test/java/org/web3j/generated/Fibonacci.java
+++ integration-tests/src/test/java/org/web3j/generated/Fibonacci.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.web3j.abi.EventEncoder;
-import org.web3j.abi.EventValues;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Event;
 import org.web3j.abi.datatypes.Function;
@@ -31,10 +30,15 @@ import rx.functions.Func1;
  * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
  * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
  *
- * <p>Generated with web3j version 3.2.0.
+ * <p>Generated with web3j version 3.3.0.
  */
 public class Fibonacci extends Contract {
-    private static final String BINARY = "6060604052341561000f57600080fd5b6101498061001e6000396000f30060606040526004361061004b5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416633c7fdc70811461005057806361047ff414610078575b600080fd5b341561005b57600080fd5b61006660043561008e565b60405190815260200160405180910390f35b341561008357600080fd5b6100666004356100da565b6000610099826100da565b90507f71e71a8458267085d5ab16980fd5f114d2d37f232479c245d523ce8d23ca40ed828260405191825260208201526040908101905180910390a1919050565b60008115156100eb57506000610118565b81600114156100fc57506001610118565b610108600283036100da565b610114600184036100da565b0190505b9190505600a165627a7a72305820e408d0180959faf2f0168a105318c66ad9cee6a322569be9157dfc267cf450ce0029";
+    private static final String BINARY = "6060604052341561000f57600080fd5b6101498061001e6000396000f30060606040526004361061004b5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416633c7fdc70811461005057806361047ff414610078575b600080fd5b341561005b57600080fd5b61006660043561008e565b60405190815260200160405180910390f35b341561008357600080fd5b6100666004356100da565b6000610099826100da565b90507f71e71a8458267085d5ab16980fd5f114d2d37f232479c245d523ce8d23ca40ed828260405191825260208201526040908101905180910390a1919050565b60008115156100eb57506000610118565b81600114156100fc57506001610118565b610108600283036100da565b610114600184036100da565b0190505b9190505600a165627a7a72305820b79593e85095f5c09ecad21be0e9501a5528960f09cf22e8543ff6221976ea8e0029";
+
+    public static final Event NOTIFY_EVENT = new Event("Notify", 
+            Arrays.<TypeReference<?>>asList(),
+            Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}, new TypeReference<Uint256>() {}));
+    ;
 
     protected Fibonacci(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
         super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
@@ -45,14 +49,11 @@ public class Fibonacci extends Contract {
     }
 
     public List<NotifyEventResponse> getNotifyEvents(TransactionReceipt transactionReceipt) {
-        final Event event = new Event("Notify", 
-                Arrays.<TypeReference<?>>asList(),
-                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}, new TypeReference<Uint256>() {}));
-        List<EventValues> valueList = extractEventParameters(event, transactionReceipt);
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(NOTIFY_EVENT, transactionReceipt);
         ArrayList<NotifyEventResponse> responses = new ArrayList<NotifyEventResponse>(valueList.size());
-        for (EventValues eventValues : valueList) {
+        for (Contract.EventValuesWithLog eventValues : valueList) {
             NotifyEventResponse typedResponse = new NotifyEventResponse();
-            typedResponse.log = transactionReceipt.getLogs().get(valueList.indexOf(eventValues));
+            typedResponse.log = eventValues.getLog();
             typedResponse.input = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
             typedResponse.result = (BigInteger) eventValues.getNonIndexedValues().get(1).getValue();
             responses.add(typedResponse);
@@ -60,16 +61,11 @@ public class Fibonacci extends Contract {
         return responses;
     }
 
-    public Observable<NotifyEventResponse> notifyEventObservable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
-        final Event event = new Event("Notify", 
-                Arrays.<TypeReference<?>>asList(),
-                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}, new TypeReference<Uint256>() {}));
-        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
-        filter.addSingleTopic(EventEncoder.encode(event));
+    public Observable<NotifyEventResponse> notifyEventObservable(EthFilter filter) {
         return web3j.ethLogObservable(filter).map(new Func1<Log, NotifyEventResponse>() {
             @Override
             public NotifyEventResponse call(Log log) {
-                EventValues eventValues = extractEventParameters(event, log);
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(NOTIFY_EVENT, log);
                 NotifyEventResponse typedResponse = new NotifyEventResponse();
                 typedResponse.log = log;
                 typedResponse.input = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
@@ -77,6 +73,12 @@ public class Fibonacci extends Contract {
                 return typedResponse;
             }
         });
+    }
+
+    public Observable<NotifyEventResponse> notifyEventObservable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(NOTIFY_EVENT));
+        return notifyEventObservable(filter);
     }
 
     public RemoteCall<TransactionReceipt> fibonacciNotify(BigInteger number) {
