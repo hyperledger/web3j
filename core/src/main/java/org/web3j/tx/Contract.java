d79c791bdf3f685e4e29ff60379bb25dda352288
Stuart Low <stuart.low@me.com>
2018-04-07 16:07:15 +1000

    Initial fix for #481

--- core/src/main/java/org/web3j/tx/Contract.java
+++ core/src/main/java/org/web3j/tx/Contract.java
@@ -43,7 +43,8 @@ public abstract class Contract extends ManagedTransaction {
 
     // https://www.reddit.com/r/ethereum/comments/5g8ia6/attention_miners_we_recommend_raising_gas_limit/
     public static final BigInteger GAS_LIMIT = BigInteger.valueOf(4_300_000);
-    public static final String SUCCESSFUL_TRANSACTION_STATUS = "0x1";
+    // keep the string definition but store it as an int, perhaps pointless?
+    public static final int SUCCESSFUL_TRANSACTION_STATUS = Integer.parseInt("0x1", 16);
 
     protected final String contractBinary;
     protected String contractAddress;
@@ -255,7 +256,7 @@ public abstract class Contract extends ManagedTransaction {
         TransactionReceipt receipt = send(contractAddress, data, weiValue, gasPrice, gasLimit);
 
         if (receipt.getStatus() != null
-                && !SUCCESSFUL_TRANSACTION_STATUS.equals(receipt.getStatus())) {
+                && SUCCESSFUL_TRANSACTION_STATUS != Integer.parseInt(receipt.getStatus(), 16)) {
             throw new TransactionException(
                     String.format(
                             "Transaction has failed with status: %s. "
