bdf70ec3682e11e79cdb9f103ce6fde0354e2951
Vojta Drabek <vojta.drabek@gmail.com>
2018-04-15 17:07:57 +0200

    Retain order of imports

--- crypto/src/main/java/org/web3j/crypto/MnemonicUtils.java
+++ crypto/src/main/java/org/web3j/crypto/MnemonicUtils.java
@@ -1,9 +1,5 @@
 package org.web3j.crypto;
 
-import org.bouncycastle.crypto.digests.SHA512Digest;
-import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
-import org.bouncycastle.crypto.params.KeyParameter;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +7,10 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.bouncycastle.crypto.digests.SHA512Digest;
+import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
+import org.bouncycastle.crypto.params.KeyParameter;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.web3j.crypto.Hash.sha256;
