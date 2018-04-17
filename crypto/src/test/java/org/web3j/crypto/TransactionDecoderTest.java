48e0decb330c80b9cf0f94805bc31e2fcdfdb368
Frank Cornelis <info@e-contract.be>
2018-04-17 16:34:43 +0200

    padding fix for r and s

--- crypto/src/test/java/org/web3j/crypto/TransactionDecoderTest.java
+++ crypto/src/test/java/org/web3j/crypto/TransactionDecoderTest.java
@@ -96,4 +96,14 @@ public class TransactionDecoderTest {
         signedResult.verify(SampleKeys.ADDRESS);
         assertEquals(chainId, signedResult.getChainId());
     }
+
+    @Test
+    public void testRSize31() throws Exception {
+        //CHECKSTYLE:OFF
+        String hexTransaction = "0xf883370183419ce09433c98f20dd73d7bb1d533c4aa3371f2b30c6ebde80a45093dc7d00000000000000000000000000000000000000000000000000000000000000351c9fb90996c836fb34b782ee3d6efa9e2c79a75b277c014e353b51b23b00524d2da07435ebebca627a51a863bf590aff911c4746ab8386a0477c8221bb89671a5d58";
+        //CHECKSTYLE:ON
+        RawTransaction result = TransactionDecoder.decode(hexTransaction);
+        SignedRawTransaction signedResult = (SignedRawTransaction) result;
+        assertEquals("0x1b609b03e2e9b0275a61fa5c69a8f32550285536", signedResult.getFrom());
+    }
 }
