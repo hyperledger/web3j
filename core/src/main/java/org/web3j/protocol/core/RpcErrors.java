b6109130ae2a733747c851ee9cd35b3c0b838d95
msavchuk <uper4206@gmail.com>
2018-06-23 15:47:53 +0300

    eth_getFilterChanges should rebuild the filter if the filter is invalid #359

---
+++ core/src/main/java/org/web3j/protocol/core/RpcErrors.java
@@ -0,0 +1,13 @@
+package org.web3j.protocol.core;
+
+public interface RpcErrors {
+    int FILTER_NOT_FOUND = -32000;
+
+    int PARSE_ERROR = -32700;
+
+    int INVALID_REQUEST = -32601;
+
+    int INVALID_PARAMS = -32602;
+
+    int INTERNAL_ERROR = -32603;
+}
