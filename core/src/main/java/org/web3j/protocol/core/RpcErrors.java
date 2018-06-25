97430efa4338bbeef6bac505be8f4a1723bd1054
msavchuk <uper4206@gmail.com>
2018-06-25 09:43:14 +0300

    Rebuild the filter if the filter is invalid #359

---
+++ core/src/main/java/org/web3j/protocol/core/RpcErrors.java
@@ -0,0 +1,15 @@
+package org.web3j.protocol.core;
+
+public final class RpcErrors {
+    public final static int FILTER_NOT_FOUND = -32000;
+
+    public final static int PARSE_ERROR = -32700;
+
+    public final static int INVALID_REQUEST = -32601;
+
+    public final static int INVALID_PARAMS = -32602;
+
+    public final static int INTERNAL_ERROR = -32603;
+
+    private RpcErrors() {}
+}
