441cc08614579df60bb323b094f1cf745a6f759a
Franz Allan V. See <franz@see.net.ph>
2018-07-18 02:58:50 +0800

    EnsIT - Fixed by adding assumption that the server it connects to is not syncing still and updated the resolved String of 'web3j.test'

--- integration-tests/src/test/java/org/web3j/ens/EnsIT.java
+++ integration-tests/src/test/java/org/web3j/ens/EnsIT.java
@@ -3,20 +3,32 @@ package org.web3j.ens;
 import org.junit.Test;
 
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.TestURL;
 import org.web3j.protocol.http.HttpService;
 
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
 public class EnsIT {
 
     @Test
     public void testEns() throws Exception {
 
-        Web3j web3j = Web3j.build(new HttpService());
-        EnsResolver ensResolver = new EnsResolver(web3j);
+        Web3j web3j = Web3j.build(new HttpService(TestURL.URL));
 
+        assumeThat("Skipping testEns() because we are still syncing, which means we will NOT be "
+                        + "able to accurately do EnsResolver#resolve()",
+                web3j.ethSyncing().send().isSyncing(),
+                allOf(
+                        notNullValue(),
+                        is(false)
+                ));
+
+        EnsResolver ensResolver = new EnsResolver(web3j);
         assertThat(ensResolver.resolve("web3j.test"),
-                is("0x19e03255f667bdfd50a32722df860b1eeaf4d635"));
+                is("0xe7fdbbac9325508269d772982cc68103f79d9693"));
     }
 }
