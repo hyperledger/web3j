6ee51bb734b4ebfe10d8cc8752ea447e0b6b1dd5
Franz Allan V. See <franz@see.net.ph>
2018-08-04 19:32:50 +0800

    Created BalanceHelper to help redistribute the balances of ALICE and BOB equally if in case one of them got to depleted.

---
+++ integration-tests/src/test/java/org/web3j/protocol/scenarios/BalanceHelper.java
@@ -0,0 +1,87 @@
+package org.web3j.protocol.scenarios;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.admin.Admin;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.TestParameters;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Convert;
+
+import static org.web3j.protocol.scenarios.Scenario.ALICE;
+import static org.web3j.protocol.scenarios.Scenario.BOB;
+
+/**
+ * This rebalances the balance of ALICE and BOB equally amongst themselves.
+ *
+ * <p>The tests should be created so that any transfer from ALICE to BOB should be offset by a
+ * transfer from BOB to ALICE to minimize the likelihood that one of them would ran out of
+ * ether.
+ *
+ * <p>But if in case the balance of ALICE or BOB has become insufficient (or if in case we just
+ * want to rebalance their balances equally amongst themselves), then we can run the
+ * {@link #main(String[])} of this class manually to do that.
+ */
+public class BalanceHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BalanceHelper.class);
+
+    public static void main(String[] args)
+            throws Exception {
+
+        LOGGER.info("Connecting to {}", TestParameters.TEST_RINKEBY_URL);
+
+        Admin web3j = Admin.build(new HttpService(TestParameters.TEST_RINKEBY_URL));
+
+        LOGGER.info("Retrieving latest balances of ALICE and BOB");
+
+        LOGGER.info("Retrieving latest balance of ALICE ({})", ALICE.getAddress());
+        BigInteger aliceBalance = web3j.ethGetBalance(
+                ALICE.getAddress(), DefaultBlockParameter.valueOf("latest")).send()
+                .getBalance();
+        LOGGER.info("ACLICE's latest balance = {}", aliceBalance);
+
+        LOGGER.info("Retrieving latest balance of BOB ({})", BOB.getAddress());
+        BigInteger bobBalance = web3j.ethGetBalance(
+                BOB.getAddress(), DefaultBlockParameter.valueOf("latest")).send()
+                .getBalance();
+        LOGGER.info("BOB's latest balance = {}", bobBalance);
+
+        BigInteger total = aliceBalance.add(bobBalance);
+        BigInteger equalBalance = total.divide(BigInteger.valueOf(2));
+
+        LOGGER.info("Total balance = {}", total);
+        LOGGER.info("Equal balance = {}", equalBalance);
+
+        Credentials sender = aliceBalance.compareTo(equalBalance) > 0 ? ALICE : BOB;
+        Credentials recipient = sender == ALICE ? BOB : ALICE;
+
+        BigInteger sendersBalance = sender == ALICE ? aliceBalance : bobBalance;
+        BigDecimal transferAmount = new BigDecimal(sendersBalance.subtract(equalBalance));
+
+        LOGGER.info("Transferring {} WEI from {} ({}) to {} ({})",
+                transferAmount,
+                sender == ALICE ? "ALICE" : "BOB",
+                sender.getAddress(),
+                recipient == ALICE ? "ALICE" : "BOB",
+                recipient.getAddress()
+        );
+
+        TransactionReceipt transactionReceipt = Transfer.sendFunds(
+                web3j,
+                sender,
+                recipient.getAddress(),
+                transferAmount,
+                Convert.Unit.WEI)
+                .send();
+
+        LOGGER.info("TransactionReceipt = {}", transactionReceipt);
+    }
+}
