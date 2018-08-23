7edef0b3a6f9f2ab09753cbe4868c84f5b9bc4fb
Daniel Schweiger <daniel.schweiger@coinfinity.co>
2018-08-23 09:21:20 +0200

    make methods public

--- crypto/src/main/java/org/web3j/crypto/TransactionEncoder.java
+++ crypto/src/main/java/org/web3j/crypto/TransactionEncoder.java
@@ -52,7 +52,7 @@ public class TransactionEncoder {
         return encode(rawTransaction, signatureData);
     }
 
-    private static byte[] encode(RawTransaction rawTransaction, Sign.SignatureData signatureData) {
+    public static byte[] encode(RawTransaction rawTransaction, Sign.SignatureData signatureData) {
         List<RlpType> values = asRlpValues(rawTransaction, signatureData);
         RlpList rlpList = new RlpList(values);
         return RlpEncoder.encode(rlpList);
