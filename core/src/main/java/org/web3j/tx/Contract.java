586d3e4dcfc846af2edda05a8f5f37459c51d513
ivaylo <iikirilov@gmail.com>
2018-10-04 14:23:57 +0100

    Fix function throws

--- core/src/main/java/org/web3j/tx/Contract.java
+++ core/src/main/java/org/web3j/tx/Contract.java
@@ -404,7 +404,7 @@ public abstract class Contract extends ManagedTransaction {
             Web3j web3j, TransactionManager transactionManager,
             BigInteger gasPrice, BigInteger gasLimit,
             String binary, String encodedConstructor, BigInteger value)
-            throws IOException, TransactionException {
+            throws RuntimeException, TransactionException {
 
         return deploy(type, web3j, transactionManager,
                 new StaticGasProvider(gasPrice, gasLimit),
