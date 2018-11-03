682a6f2b86de5f1740a960fc799da5042fc895b6
honza <janbenes1234@gmail.com>
2018-11-03 15:25:27 +0100

    Implementation of minerStart and minerStop method, which are similar to geth ones

--- pantheon/src/main/java/org/web3j/protocol/pantheon/JsonRpc2_0Pantheon.java
+++ pantheon/src/main/java/org/web3j/protocol/pantheon/JsonRpc2_0Pantheon.java
@@ -1,10 +1,33 @@
 package org.web3j.protocol.pantheon;
 
 import org.web3j.protocol.Web3jService;
+import org.web3j.protocol.admin.methods.response.BooleanResponse;
 import org.web3j.protocol.core.JsonRpc2_0Web3j;
+import org.web3j.protocol.core.Request;
+import org.web3j.protocol.core.methods.response.MinerStartResponse;
+
+import java.util.Collections;
 
 public class JsonRpc2_0Pantheon extends JsonRpc2_0Web3j implements Pantheon {
     public JsonRpc2_0Pantheon(Web3jService web3jService) {
         super(web3jService);
+    }
+
+    @Override
+    public Request<?, MinerStartResponse> minerStart() {
+        return new Request<>(
+                "miner_start",
+                Collections.<String>emptyList(),
+                web3jService,
+                MinerStartResponse.class);
+    }
+
+    @Override
+    public Request<?, BooleanResponse> minerStop() {
+        return new Request<>(
+                "miner_stop",
+                Collections.<String>emptyList(),
+                web3jService,
+                BooleanResponse.class);
     }
 }
--- pantheon/src/main/java/org/web3j/protocol/pantheon/Pantheon.java
+++ pantheon/src/main/java/org/web3j/protocol/pantheon/Pantheon.java
@@ -2,9 +2,16 @@ package org.web3j.protocol.pantheon;
 
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.Web3jService;
+import org.web3j.protocol.admin.methods.response.BooleanResponse;
+import org.web3j.protocol.core.Request;
+import org.web3j.protocol.core.methods.response.MinerStartResponse;
 
 public interface Pantheon extends Web3j {
     static Pantheon build(Web3jService web3jService) {
         return new JsonRpc2_0Pantheon(web3jService);
     }
+
+    Request<?, MinerStartResponse> minerStart();
+
+    Request<?, BooleanResponse> minerStop();
 }
