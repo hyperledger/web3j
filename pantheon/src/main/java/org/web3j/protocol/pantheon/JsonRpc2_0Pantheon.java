75e9bc0b6eb6e0c897743a68bd168a7267f02e8e
honza <janbenes1234@gmail.com>
2018-11-03 15:06:21 +0100

    Implementation of Web3j interface by extension of JsonRpc2_0Web3j class

--- pantheon/src/main/java/org/web3j/protocol/pantheon/JsonRpc2_0Pantheon.java
+++ pantheon/src/main/java/org/web3j/protocol/pantheon/JsonRpc2_0Pantheon.java
@@ -1,4 +1,10 @@
 package org.web3j.protocol.pantheon;
 
-public class JsonRpc2_0Pantheon implements Pantheon {
+import org.web3j.protocol.Web3jService;
+import org.web3j.protocol.core.JsonRpc2_0Web3j;
+
+public class JsonRpc2_0Pantheon extends JsonRpc2_0Web3j implements Pantheon {
+    public JsonRpc2_0Pantheon(Web3jService web3jService) {
+        super(web3jService);
+    }
 }
--- pantheon/src/main/java/org/web3j/protocol/pantheon/Pantheon.java
+++ pantheon/src/main/java/org/web3j/protocol/pantheon/Pantheon.java
@@ -1,4 +1,10 @@
 package org.web3j.protocol.pantheon;
 
-public interface Pantheon {
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jService;
+
+public interface Pantheon extends Web3j {
+    static Pantheon build(Web3jService web3jService) {
+        return new JsonRpc2_0Pantheon(web3jService);
+    }
 }
