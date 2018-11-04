601b2cea119aa8bb4a5f353db3f55ab0b2bd765a
Robert Brada <bert.blader@gmail.com>
2018-11-04 13:46:01 +0100

    added debug_traceTransaction RequestTest

--- pantheon/src/test/java/org/web3j/protocol/pantheon/RequestTest.java
+++ pantheon/src/test/java/org/web3j/protocol/pantheon/RequestTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.web3j.protocol.RequestTester;
 import org.web3j.protocol.http.HttpService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestTest extends RequestTester {
     private Pantheon web3j;
 
@@ -26,5 +29,28 @@ public class RequestTest extends RequestTester {
 
         verifyResult("{\"jsonrpc\":\"2.0\",\"method\":\"miner_stop\","
                 + "\"params\":[],\"id\":1}");
+    }
+
+    @Test
+    public void testTraceTransaction() throws Exception {
+
+        String transactionHash = "0xc171033d5cbff7175f29dfd3a63dda3d6f8f385e";
+
+        Map <String,Boolean> options=  new HashMap <>();
+        options.put("disableStorage", false);
+        options.put("disableStack",false);
+        options.put("disableMemory",true);
+
+        web3j.debugTraceTransaction(transactionHash, options).send();
+
+        //CHECKSTYLE:OFF
+        verifyResult("{\"jsonrpc\":\"2.0\",\"method\":\"debug_traceTransaction\","
+                + "\"params\":[\"0xc171033d5cbff7175f29dfd3a63dda3d6f8f385e\","
+                + "{\"disableMemory\":true,"
+                + "\"disableStorage\":false,"
+                + "\"disableStack\":false}],"
+                + "\"id\":1}");
+        //CHECKSTYLE:ON
+
     }
 }
