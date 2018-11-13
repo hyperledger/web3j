ba32d693a3ca56efde0873f99f0091e20c3ae6e5
sebaraba <rabasebastians@gmail.com>
2018-11-13 14:09:40 +0000

    Fix checkstyle problems for merge

--- core/src/main/java/org/web3j/protocol/core/DefaultBlockParameter.java
+++ core/src/main/java/org/web3j/protocol/core/DefaultBlockParameter.java
@@ -10,8 +10,9 @@ import java.math.BigInteger;
  */
 public interface DefaultBlockParameter {
     static DefaultBlockParameter valueOf(BigInteger blockNumber) {
-        if (BigInteger.ZERO.compareTo(blockNumber) >= 0)
+        if (BigInteger.ZERO.compareTo(blockNumber) >= 0) {
             blockNumber = BigInteger.ZERO;
+        }
         return new DefaultBlockParameterNumber(blockNumber);
     }
 
