a54de5d59c3e74dcee7173f6544d359eae593f9c
Sam Nazha <sam@blk.io>
2018-11-15 08:03:15 +1100

    768: pulled in the initial docs by mushketyk, with some minor changes

--- core/src/test/java/org/web3j/protocol/websocket/WebSocketServiceTest.java
+++ core/src/test/java/org/web3j/protocol/websocket/WebSocketServiceTest.java
@@ -21,7 +21,6 @@ import org.junit.rules.ExpectedException;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
-import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.Response;
 import org.web3j.protocol.core.methods.response.EthSubscribe;
@@ -59,11 +58,6 @@ public class WebSocketServiceTest {
     public ExpectedException thrown = ExpectedException.none();
     private Request<Object, EthSubscribe> subscribeRequest;
 
-    public void d() {
-        final Web3j web3j = Web3j.build(service);
-        final Flowable<NewHeadsNotification> notifications = web3j.newHeadsNotifications();
-
-    }
     @Before
     public void before() throws InterruptedException {
         when(webSocketClient.connectBlocking()).thenReturn(true);
