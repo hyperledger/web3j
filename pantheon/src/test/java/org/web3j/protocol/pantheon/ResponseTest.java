8c2d7e7d9567e39d36cc5bc7b88c6deb76ca6bd4
Sam Nazha <sam@blk.io>
2018-11-15 21:18:53 +1100

    767: checkstyle fixes

--- pantheon/src/test/java/org/web3j/protocol/pantheon/ResponseTest.java
+++ pantheon/src/test/java/org/web3j/protocol/pantheon/ResponseTest.java
@@ -1,6 +1,7 @@
 package org.web3j.protocol.pantheon;
 
 import org.junit.Test;
+
 import org.web3j.protocol.ResponseTester;
 import org.web3j.protocol.core.methods.response.EthAccounts;
 import org.web3j.protocol.pantheon.response.PantheonEthAccountsMapResponse;
@@ -15,13 +16,17 @@ public class ResponseTest extends ResponseTester {
         buildResponse("{\n"
                 + "    \"jsonrpc\": \"2.0\",\n"
                 + "    \"id\": 1,\n"
-                + "    \"result\": [\"0x42eb768f2244c8811c63729a21a3569731535f06\",\"0x7ffc57839b00206d1ad20c69a1981b489f772031\",\"0xb279182d99e65703f0076e4812653aab85fca0f0\"]\n"
+                + "    \"result\": [\"0x42eb768f2244c8811c63729a21a3569731535f06\","
+                + "\"0x7ffc57839b00206d1ad20c69a1981b489f772031\","
+                + "\"0xb279182d99e65703f0076e4812653aab85fca0f0\"]\n"
                 + "}");
 
         EthAccounts ethAccounts = deserialiseResponse(
                 EthAccounts.class);
         assertThat(ethAccounts.getAccounts().toString(),
-                is("[0x42eb768f2244c8811c63729a21a3569731535f06, 0x7ffc57839b00206d1ad20c69a1981b489f772031, 0xb279182d99e65703f0076e4812653aab85fca0f0]"));
+                is("[0x42eb768f2244c8811c63729a21a3569731535f06, "
+                        + "0x7ffc57839b00206d1ad20c69a1981b489f772031, "
+                        + "0xb279182d99e65703f0076e4812653aab85fca0f0]"));
     }
 
     @Test
@@ -29,12 +34,13 @@ public class ResponseTest extends ResponseTester {
         buildResponse("{\n"
                 + "    \"jsonrpc\": \"2.0\",\n"
                 + "    \"id\": 1,\n"
-                + "    \"result\": {\"0x42eb768f2244c8811c63729a21a3569731535f07\": false,\"0x12eb759f2222d7711c63729a45c3585731521d01\": true}\n"
-                + "}");
+                + "    \"result\": {\"0x42eb768f2244c8811c63729a21a3569731535f07\": false,"
+                + "\"0x12eb759f2222d7711c63729a45c3585731521d01\": true}\n}");
 
         PantheonEthAccountsMapResponse mapResponse = deserialiseResponse(
                 PantheonEthAccountsMapResponse.class);
         assertThat(mapResponse.getAccounts().toString(),
-                is("{0x42eb768f2244c8811c63729a21a3569731535f07=false, 0x12eb759f2222d7711c63729a45c3585731521d01=true}"));
+                is("{0x42eb768f2244c8811c63729a21a3569731535f07=false, "
+                        + "0x12eb759f2222d7711c63729a45c3585731521d01=true}"));
     }
 }
