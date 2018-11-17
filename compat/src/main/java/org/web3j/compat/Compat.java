17518f674926385d080115b60a396d531bdad3cb
Sergey Solovyev <se.solovyev@gmail.com>
2018-11-17 15:30:48 +0100

    Web3a: Introduce compatibility submodule to deal with Android integration
    This commit adds JDK jars used by Android during compilation:
    android.jar and core-lambda-subs.jar.
    
    It also adds a buildscript method that makes a submodule be compiled against
    this (reduced) JDK. All submodules that are intended to be used on Android
    should call this method:
      useReducedJdk()
    Such submodules won't be able to use languge APIs (packages, classes and methods)
    that are not available on Android.

---
+++ compat/src/main/java/org/web3j/compat/Compat.java
@@ -0,0 +1,11 @@
+package org.web3j.compat;
+
+/**
+ * Provides compatibility methods to substitute those methods from Java SDK that
+ * are not supported on Android.
+ */
+public final class Compat {
+
+    private Compat() {
+    }
+}
