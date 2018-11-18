967b3e46089d595934b648fd9e83b3803c79a77e
Sergey Solovyev <se.solovyev@gmail.com>
2018-11-18 11:31:32 +0100

    Web3a: Port String#join from Java 8 and use it in Strings

--- compat/src/main/java/org/web3j/compat/Compat.java
+++ compat/src/main/java/org/web3j/compat/Compat.java
@@ -8,4 +8,21 @@ public final class Compat {
 
     private Compat() {
     }
+
+    /**
+     * Ports {@link String#join(CharSequence, CharSequence...)}.
+     */
+    public static String join(CharSequence delimiter, CharSequence... elements) {
+        if (elements.length == 0) {
+            return "";
+        }
+        final StringBuilder sb = new StringBuilder(7 * elements.length);
+        sb.append(elements[0]);
+        for (int i = 1; i < elements.length; i++) {
+            sb.append(delimiter);
+            sb.append(elements[i]);
+
+        }
+        return sb.toString();
+    }
 }
