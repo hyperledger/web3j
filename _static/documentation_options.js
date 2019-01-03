db5d2328052027bcb2d094753c5d4c72d73d7156
Conor <conor10@gmail.com>
2019-01-03 10:07:18 +0000

    Updated docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '3.4.0',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '4.0.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
 };
