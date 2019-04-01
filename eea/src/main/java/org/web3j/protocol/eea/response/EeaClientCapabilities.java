0f245a5ab5c38dc39c2be2ad943170a959add3ad
ivaylo <iikirilov@gmail.com>
2019-04-01 11:58:57 +0100

    Stubbed eea objects used for pantheon private transactions

---
+++ eea/src/main/java/org/web3j/protocol/eea/response/EeaClientCapabilities.java
@@ -0,0 +1,7 @@
+package org.web3j.protocol.eea.response;
+
+import org.web3j.protocol.core.Response;
+import org.web3j.protocol.eea.response.object.ClientCapabilities;
+
+public class EeaClientCapabilities extends Response<ClientCapabilities> {
+}
---
+++ eea/src/main/java/org/web3j/protocol/eea/response/object/ClientCapabilities.java
@@ -0,0 +1,4 @@
+package org.web3j.protocol.eea.response.object;
+
+public class ClientCapabilities {
+}
