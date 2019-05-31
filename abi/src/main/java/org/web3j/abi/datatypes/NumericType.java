18c498a840397811749969ec7bbe59e4482a0067
Xavier <xavier@blk.io>
2019-05-31 11:11:26 +0100

    Add bit size getter on NumericType

--- abi/src/main/java/org/web3j/abi/datatypes/NumericType.java
+++ abi/src/main/java/org/web3j/abi/datatypes/NumericType.java
@@ -35,6 +35,8 @@ public abstract class NumericType implements Type<BigInteger> {
         return value;
     }
 
+    public abstract int getBitSize();
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
