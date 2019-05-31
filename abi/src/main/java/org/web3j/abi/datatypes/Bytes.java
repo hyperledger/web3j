7ba1850353e32ecea171ec6cdba51f2e4788ee39
Xavier <xavier@blk.io>
2019-05-31 11:57:35 +0100

    Make BytesType abstract for consistency with datatype model

--- abi/src/main/java/org/web3j/abi/datatypes/Bytes.java
+++ abi/src/main/java/org/web3j/abi/datatypes/Bytes.java
@@ -19,14 +19,14 @@ public class Bytes extends BytesType {
 
     protected Bytes(int byteSize, byte[] value) {
         super(value, TYPE_NAME + value.length);
-        if (!isValid(byteSize, value)) {
+        if (!isValid(byteSize)) {
             throw new UnsupportedOperationException(
                     "Input byte array must be in range 0 < M <= 32 and length must match type");
         }
     }
 
-    private boolean isValid(int byteSize, byte[] value) {
-        int length = value.length;
+    private boolean isValid(int byteSize) {
+        int length = getValue().length;
         return length > 0 && length <= 32 && length == byteSize;
     }
 }
