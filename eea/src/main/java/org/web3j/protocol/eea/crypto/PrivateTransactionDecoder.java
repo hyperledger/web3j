aad955b1fae363284c767b7ebf071f0c2134bafa
Ivaylo Kirilov <iikirilov@gmail.com>
2019-07-21 09:11:35 +0100

    fix tests

--- eea/src/main/java/org/web3j/protocol/eea/crypto/PrivateTransactionDecoder.java
+++ eea/src/main/java/org/web3j/protocol/eea/crypto/PrivateTransactionDecoder.java
@@ -21,6 +21,7 @@ import org.web3j.crypto.TransactionDecoder;
 import org.web3j.rlp.RlpDecoder;
 import org.web3j.rlp.RlpList;
 import org.web3j.rlp.RlpString;
+import org.web3j.rlp.RlpType;
 import org.web3j.utils.Numeric;
 
 public class PrivateTransactionDecoder {
@@ -28,62 +29,66 @@ public class PrivateTransactionDecoder {
     public static RawPrivateTransaction decode(final String hexTransaction) {
         final byte[] transaction = Numeric.hexStringToByteArray(hexTransaction);
         final RlpList rlpList = RlpDecoder.decode(transaction);
-        final RlpList values = (RlpList) rlpList.getValues().get(0);
+        final RlpList temp = (RlpList) rlpList.getValues().get(0);
+        final List<RlpType> values = temp.getValues();
 
         final RawTransaction rawTransaction = TransactionDecoder.decode(hexTransaction);
 
-        if (values.getValues().size() == 8) {
+        if (values.size() == 8) {
             return new RawPrivateTransaction(rawTransaction,
-                    extractString(values, 6),  extractString(values, 7));
-        } else if (values.getValues().size() == 9) {
-            final String privateFrom = extractString(values, 6);
-            final String restriction = extractString(values, 8);
-            if (values.getValues().get(7) instanceof RlpList) {
+                    extractBase64(values.get(6)),  extractString(values.get(7)));
+        } else if (values.size() == 9) {
+            final String privateFrom = extractBase64(values.get(6));
+            final String restriction = extractString(values.get(8));
+            if (values.get(7) instanceof RlpList) {
                 return new RawPrivateTransaction(
                         rawTransaction,
                         privateFrom,
-                        extractStringList(values, 7),
+                        extractBase64List((RlpList) values.get(7)),
                         restriction);
             } else {
                 return new RawPrivateTransaction(
                         rawTransaction,
                         privateFrom,
-                        extractString(values, 7),
+                        extractBase64(values.get(7)),
                         restriction);
             }
 
-        } else if (values.getValues().size() == 11) {
+        } else if (values.size() == 11) {
             return new SignedRawPrivateTransaction(
                     (SignedRawTransaction) rawTransaction,
-                    extractString(values, 9),
-                    extractString(values, 10));
+                    extractBase64(values.get(9)),
+                    extractString(values.get(10)));
         } else {
-            final String privateFrom = extractString(values, 9);
-            final String restriction = extractString(values, 11);
-            if (values.getValues().get(10) instanceof RlpList) {
+            final String privateFrom = extractBase64(values.get(9));
+            final String restriction = extractString(values.get(11));
+            if (values.get(10) instanceof RlpList) {
                 return new SignedRawPrivateTransaction(
                         (SignedRawTransaction) rawTransaction,
                         privateFrom,
-                        extractStringList(values, 10),
+                        extractBase64List(values.get(10)),
                         restriction);
             } else {
                 return new SignedRawPrivateTransaction(
                         (SignedRawTransaction) rawTransaction,
                         privateFrom,
-                        extractString(values, 10),
+                        extractBase64(values.get(10)),
                         restriction);
             }
         }
     }
 
-    private static String extractString(final RlpList values, int i) {
-        return new String(((RlpString) values.getValues().get(i)).getBytes());
+    private static String extractString(final RlpType value) {
+        return new String(((RlpString) value).getBytes());
     }
 
-    private static List<String> extractStringList(final RlpList values, int i) {
-        return ((RlpList) values.getValues().get(i))
-                .getValues().stream()
-                        .map(rlp -> new String(((RlpString) rlp).getBytes()))
+    private static String extractBase64(final RlpType value) {
+        return Numeric.byteArrayToBase64(((RlpString) value).getBytes());
+    }
+
+    private static List<String> extractBase64List(final RlpType values) {
+        return ((RlpList) values).getValues().stream()
+                        .map(PrivateTransactionDecoder::extractBase64)
                         .collect(Collectors.toList());
     }
 }
