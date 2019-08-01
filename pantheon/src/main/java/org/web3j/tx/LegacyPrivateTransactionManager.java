9f476fbc1a777211a9c58cce47786330a5805de3
Ivaylo Kirilov <iikirilov@gmail.com>
2019-08-01 11:44:52 +0100

    Expose legacy privacy group generation

--- pantheon/src/main/java/org/web3j/tx/LegacyPrivateTransactionManager.java
+++ pantheon/src/main/java/org/web3j/tx/LegacyPrivateTransactionManager.java
@@ -12,22 +12,12 @@
  */
 package org.web3j.tx;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.web3j.crypto.Credentials;
-import org.web3j.crypto.Hash;
 import org.web3j.protocol.pantheon.Pantheon;
-import org.web3j.rlp.RlpEncoder;
-import org.web3j.rlp.RlpList;
-import org.web3j.rlp.RlpString;
-import org.web3j.rlp.RlpType;
 import org.web3j.tx.gas.PantheonPrivacyGasProvider;
-import org.web3j.utils.Numeric;
+import org.web3j.utils.PrivacyGroupUtils;
 
 /** PrivateTransactionManager implementation for using a Pantheon node to transact. */
 public class LegacyPrivateTransactionManager extends PrivateTransactionManager {
@@ -46,7 +36,7 @@ public class LegacyPrivateTransactionManager extends PrivateTransactionManager {
             final int sleepDuration) {
         super(pantheon, gasProvider, credentials, chainId, privateFrom, attempts, sleepDuration);
         this.privateFor = privateFor;
-        this.privacyGroupId = generatePrivacyGroupId(privateFrom, privateFor);
+        this.privacyGroupId = PrivacyGroupUtils.generateLegacyGroup(privateFrom, privateFor);
     }
 
     public LegacyPrivateTransactionManager(
@@ -74,20 +64,5 @@ public class LegacyPrivateTransactionManager extends PrivateTransactionManager {
     @Override
     Object privacyGroupIdOrPrivateFor() {
         return privateFor;
-    }
-
-    private String generatePrivacyGroupId(final String privateFrom, final List<String> privateFor) {
-        final List<byte[]> stringList = new ArrayList<>();
-        stringList.add(Base64.getDecoder().decode(privateFrom));
-        privateFor.forEach(item -> stringList.add(Base64.getDecoder().decode(item)));
-
-        final List<RlpType> rlpList =
-                stringList.stream()
-                        .distinct()
-                        .sorted(Comparator.comparing(Arrays::hashCode))
-                        .map(RlpString::create)
-                        .collect(Collectors.toList());
-
-        return Numeric.byteArrayToBase64(Hash.sha3(RlpEncoder.encode(new RlpList(rlpList))));
     }
 }
