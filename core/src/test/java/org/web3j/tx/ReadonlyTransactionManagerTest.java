18cd0aade35433bb7ffa4a4fbc7aeb0f77d199c1
electro-lite <alexandrosrousos@gmail.com>
2019-08-08 17:52:23 +0100

    Removed unused objects

--- core/src/test/java/org/web3j/tx/ReadonlyTransactionManagerTest.java
+++ core/src/test/java/org/web3j/tx/ReadonlyTransactionManagerTest.java
@@ -14,15 +14,12 @@ package org.web3j.tx;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Arrays;
 
 import org.junit.Test;
 
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.Web3jService;
 import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.protocol.core.Request;
-import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.protocol.core.methods.response.EthCall;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -35,16 +32,8 @@ public class ReadonlyTransactionManagerTest {
 
     Web3jService service = mock(Web3jService.class);
     Web3j web3j = Web3j.build(service);
-    Transaction transaction = mock(Transaction.class);
     DefaultBlockParameter defaultBlockParameter = mock(DefaultBlockParameter.class);
     EthCall response = mock(EthCall.class);
-
-    Request<?, EthCall> request =
-            new Request<>(
-                    "eth_call",
-                    Arrays.asList(transaction, defaultBlockParameter),
-                    service,
-                    org.web3j.protocol.core.methods.response.EthCall.class);
 
     @Test
     public void sendCallTest() throws IOException {
