5df9da70d357fac9928ce79826bbf52fadb7e719
xwc1125 <xmcr1125@qq.com>
2019-08-19 14:55:43 +0800

    Solve  web3j.ethLogFlowable is invalid
    Solve  web3j.ethLogFlowable is invalid

--- core/src/main/java/org/web3j/protocol/core/methods/request/EthFilter.java
+++ core/src/main/java/org/web3j/protocol/core/methods/request/EthFilter.java
@@ -51,8 +51,11 @@ public class EthFilter extends Filter<EthFilter> {
         return toBlock;
     }
 
-    public List<String> getAddress() {
-        return address;
+    public String getAddress() {
+        if (address != null && address.size() == 1) {
+            return address.get(0);
+        }
+        return address.toString();
     }
 
     @Override
