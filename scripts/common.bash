bdc611eec28986fbed84b7b2fac99781b09bd9fc
Antony Denyer <email@antonydenyer.com>
2019-09-18 15:25:22 +0100

    Build(ci): trigger build in org.web3j.quorum by creating branch

--- scripts/common.bash
+++ scripts/common.bash
@@ -11,3 +11,14 @@ github_clone() {
     git clone https://antonydenyer:${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/web3j/$1.git
     cd $1
 }
+
+ensure_version() {
+    if [[ -z "$VERSION" ]]; then
+        VERSION="${CI_COMMIT_REF_NAME//release\/}"
+    fi
+
+    if [[ "$VERSION" = "" ]]; then
+        echo "ERROR: Missing VERSION specify it using an env variable"
+        exit 1
+    fi
+}
