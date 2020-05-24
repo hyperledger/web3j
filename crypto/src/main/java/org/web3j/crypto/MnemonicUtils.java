0790af36ca5d9cbb75f06a8ee6eb41bc23c34dfa
Ivaylo Kirilov <iikirilov@gmail.com>
2020-05-24 12:38:23 +0100

    crypto

--- crypto/src/main/java/org/web3j/crypto/MnemonicUtils.java
+++ crypto/src/main/java/org/web3j/crypto/MnemonicUtils.java
@@ -27,7 +27,7 @@ import org.bouncycastle.crypto.digests.SHA512Digest;
 import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
 import org.bouncycastle.crypto.params.KeyParameter;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.web3j.compat.Compat.UTF_8;
 import static org.web3j.crypto.Hash.sha256;
 
 /**
