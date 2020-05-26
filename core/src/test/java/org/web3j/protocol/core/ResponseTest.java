04bf562520be5d1dbf3f9934c8e84a6e1f33b2d6
Ivaylo Kirilov <iikirilov@gmail.com>
2020-05-26 17:59:24 +0100

    fix tests

--- core/src/test/java/org/web3j/protocol/core/ResponseTest.java
+++ core/src/test/java/org/web3j/protocol/core/ResponseTest.java
@@ -19,7 +19,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java8.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
