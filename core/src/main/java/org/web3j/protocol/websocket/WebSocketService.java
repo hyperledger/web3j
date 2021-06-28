6b3d5ab8f51826daf08f5432a7d6a534937fa95f
Hentai Ousama <1905612@kiit.ac.in>
2021-06-28 21:03:31 +0530

    Fixed Exception catch from Exception to NumberFormatException. Ran spotlessApply.

--- core/src/main/java/org/web3j/protocol/websocket/WebSocketService.java
+++ core/src/main/java/org/web3j/protocol/websocket/WebSocketService.java
@@ -436,12 +436,16 @@ public class WebSocketService implements Web3jService {
             if (idField.isTextual()) {
                 try {
                     return Long.parseLong(idField.asText());
-                } catch (Exception e) {
-                    throw new IOException(String.format("Textual Found 'id' that cannot be casted to long. Input : '%s'", idField.asText()));
+                } catch (NumberFormatException e) {
+                    throw new IOException(
+                            String.format(
+                                    "Found Textual 'id' that cannot be casted to long. Input : '%s'",
+                                    idField.asText()));
                 }
             } else {
                 throw new IOException(
-                        String.format("'id' expected to be long, but it is: '%s'", idField.asText()));
+                        String.format(
+                                "'id' expected to be long, but it is: '%s'", idField.asText()));
             }
         }
 
