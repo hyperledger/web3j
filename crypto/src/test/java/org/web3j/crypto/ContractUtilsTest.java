2e7aa91104ed220625b1f6fa13e3eb8fcb1179c6
Zhiyu Zhang <zhiyu.zhang@circle.com>
2021-11-12 16:10:08 -0500

    implement epi1014 create2

--- crypto/src/test/java/org/web3j/crypto/ContractUtilsTest.java
+++ crypto/src/test/java/org/web3j/crypto/ContractUtilsTest.java
@@ -12,12 +12,14 @@
  */
 package org.web3j.crypto;
 
-import java.math.BigInteger;
-
 import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.web3j.crypto.ContractUtils.generateContractAddress;
+import static org.web3j.crypto.ContractUtils.generateCreate2ContractAddress;
+import static org.web3j.utils.Numeric.hexStringToByteArray;
 
 public class ContractUtilsTest {
 
@@ -32,5 +34,65 @@ public class ContractUtilsTest {
         assertEquals(
                 generateContractAddress(address, BigInteger.valueOf(257)),
                 ("0x59c21d36fbe415218e834683cb6616f2bc971ca9"));
+    }
+
+    @Test
+    public void testEIP1014Create2ContractAddress() {
+        // https://eips.ethereum.org/EIPS/eip-1014
+        // example 0
+        assertEquals(
+                Keys.toChecksumAddress(generateCreate2ContractAddress(
+                        "0x0000000000000000000000000000000000000000",
+                        hexStringToByteArray("0x0000000000000000000000000000000000000000000000000000000000000000"),
+                        hexStringToByteArray("0x00"))),
+                ("0x4D1A2e2bB4F88F0250f26Ffff098B0b30B26BF38"));
+
+        // example 1
+        assertEquals(
+                Keys.toChecksumAddress(generateCreate2ContractAddress(
+                        "0xdeadbeef00000000000000000000000000000000",
+                        hexStringToByteArray("0x0000000000000000000000000000000000000000000000000000000000000000"),
+                        hexStringToByteArray("0x00"))),
+                ("0xB928f69Bb1D91Cd65274e3c79d8986362984fDA3"));
+
+        // example 2
+        assertEquals(
+                Keys.toChecksumAddress(generateCreate2ContractAddress(
+                        "0xdeadbeef00000000000000000000000000000000",
+                        hexStringToByteArray("0x000000000000000000000000feed000000000000000000000000000000000000"),
+                        hexStringToByteArray("0x00"))),
+                ("0xD04116cDd17beBE565EB2422F2497E06cC1C9833"));
+
+        // example 3
+        assertEquals(
+                Keys.toChecksumAddress(generateCreate2ContractAddress(
+                        "0x0000000000000000000000000000000000000000",
+                        hexStringToByteArray("0x0000000000000000000000000000000000000000000000000000000000000000"),
+                        hexStringToByteArray("0xdeadbeef"))),
+                ("0x70f2b2914A2a4b783FaEFb75f459A580616Fcb5e"));
+
+        // example 4
+        assertEquals(
+                Keys.toChecksumAddress(generateCreate2ContractAddress(
+                        "0x00000000000000000000000000000000deadbeef",
+                        hexStringToByteArray("0x00000000000000000000000000000000000000000000000000000000cafebabe"),
+                        hexStringToByteArray("0xdeadbeef"))),
+                ("0x60f3f640a8508fC6a86d45DF051962668E1e8AC7"));
+
+        // example 5
+        assertEquals(
+                Keys.toChecksumAddress(generateCreate2ContractAddress(
+                        "0x00000000000000000000000000000000deadbeef",
+                        hexStringToByteArray("0x00000000000000000000000000000000000000000000000000000000cafebabe"),
+                        hexStringToByteArray("0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"))),
+                ("0x1d8bfDC5D46DC4f61D6b6115972536eBE6A8854C"));
+
+        // example 6
+        assertEquals(
+                Keys.toChecksumAddress(generateCreate2ContractAddress(
+                        "0x0000000000000000000000000000000000000000",
+                        hexStringToByteArray("0x0000000000000000000000000000000000000000000000000000000000000000"),
+                        hexStringToByteArray("0x"))),
+                ("0xE33C0C7F7df4809055C3ebA6c09CFe4BaF1BD9e0"));
     }
 }
