7b470e5322a74cd29ae1aadd0257a6066a739c6c
andrii_kl <andriikliui@gmail.com>
2022-11-08 22:55:02 +0300

    feat-1782 run spotlessApply

--- abi/src/main/java/org/web3j/abi/TypeDecoder.java
+++ abi/src/main/java/org/web3j/abi/TypeDecoder.java
@@ -61,8 +61,9 @@ import static org.web3j.abi.Utils.staticStructNestedPublicFieldsFlatList;
  * documented, but is the reverse of the encoding details located <a
  * href="https://github.com/ethereum/wiki/wiki/Ethereum-Contract-ABI">here</a>.
  *
- * The public API is composed of "decode*" methods and provides backward-compatbility.
- * See https://github.com/web3j/web3j/issues/1591 for a discussion about decoding and possible improvements.
+ * <p>The public API is composed of "decode*" methods and provides backward-compatbility. See
+ * https://github.com/web3j/web3j/issues/1591 for a discussion about decoding and possible
+ * improvements.
  */
 public class TypeDecoder {
 
