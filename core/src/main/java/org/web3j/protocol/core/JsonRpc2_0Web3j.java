1e5aedbf80eaa2c577f87e7871241f326983b750
George Tebrean <99179176+gtebrean@users.noreply.github.com>
2022-12-22 15:35:56 +0200

    Fix ETH feeHistory  (#1834)
    * fix fee

--- core/src/main/java/org/web3j/protocol/core/JsonRpc2_0Web3j.java
+++ core/src/main/java/org/web3j/protocol/core/JsonRpc2_0Web3j.java
@@ -233,8 +233,19 @@ public class JsonRpc2_0Web3j implements Web3j {
     }
 
     @Override
+    @Deprecated
     public Request<?, EthFeeHistory> ethFeeHistory(
             int blockCount, DefaultBlockParameter newestBlock, List<Double> rewardPercentiles) {
+        return new Request<>(
+                "eth_feeHistory",
+                Arrays.asList(blockCount, newestBlock.getValue(), rewardPercentiles),
+                web3jService,
+                EthFeeHistory.class);
+    }
+
+    @Override
+    public Request<?, EthFeeHistory> ethFeeHistory(
+            String blockCount, DefaultBlockParameter newestBlock, List<Double> rewardPercentiles) {
         return new Request<>(
                 "eth_feeHistory",
                 Arrays.asList(blockCount, newestBlock.getValue(), rewardPercentiles),
