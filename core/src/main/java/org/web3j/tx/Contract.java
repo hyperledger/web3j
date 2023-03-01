55682fb016b036b69cd33d3766f994ba108c67ae
Phillip Olesen <phillip.olesen@digitalasset.com>
2023-03-01 12:45:25 +0100

    Change from wildcard importy to specific import

--- core/src/main/java/org/web3j/tx/Contract.java
+++ core/src/main/java/org/web3j/tx/Contract.java
@@ -15,7 +15,14 @@ package org.web3j.tx;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.web3j.abi.EventEncoder;
