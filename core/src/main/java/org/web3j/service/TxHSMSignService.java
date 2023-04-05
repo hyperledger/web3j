ab799448fcb7e4760929d08b9466e06ef5a844bf
gtebrean <99179176+gtebrean@users.noreply.github.com>
2023-04-05 12:10:03 +0300

    Revert change

--- core/src/main/java/org/web3j/service/TxHSMSignService.java
+++ core/src/main/java/org/web3j/service/TxHSMSignService.java
@@ -56,7 +56,6 @@ public class TxHSMSignService<T extends HSMPass> implements TxSignService {
         if (isLegacy) {
             signatureData = createEip155SignatureData(signatureData, chainId);
         }
-        // test comment
         finalBytes = encode(rawTransaction, signatureData);
 
         return finalBytes;
