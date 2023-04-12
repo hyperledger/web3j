fe78e04cac02a5f6339f473f382ce5818f4eeee2
gtebrean <99179176+gtebrean@users.noreply.github.com>
2023-04-12 12:06:58 +0300

    Update Secp256k1JWK.java

--- crypto/src/main/java/org/web3j/crypto/Secp256k1JWK.java
+++ crypto/src/main/java/org/web3j/crypto/Secp256k1JWK.java
@@ -21,8 +21,8 @@ import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 
 /**
- * This class consists in java representation of JWK file. It uses Builder pattern in order to generate
- * the JWK content from the BCEC asymetric keys receive as parameters. This class supports
+ * This class consists in java representation of JWK file. It uses Builder pattern in order to
+ * generate the JWK content from the BCEC asymetric keys receive as parameters. This class supports
  * generation of JWK only for ECDSA type keys that have secp256k1 curve.
  */
 @JsonDeserialize(builder = Secp256k1JWK.Builder.class)
