4d689755c6df01eea2bac78a96c38f06c1d89100
gtebrean <99179176+gtebrean@users.noreply.github.com>
2023-08-31 15:48:12 +0300

    Update EthCallIT.java

--- integration-tests/src/test/java/org/web3j/protocol/scenarios/EthCallIT.java
+++ integration-tests/src/test/java/org/web3j/protocol/scenarios/EthCallIT.java
@@ -35,7 +35,7 @@ import org.web3j.tx.gas.ContractGasProvider;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@EVMTest(type = NodeType.EMBEDDED)
+@EVMTest(type = NodeType.COMPOSE)
 public class EthCallIT extends Scenario {
 
     private static Revert contract;
