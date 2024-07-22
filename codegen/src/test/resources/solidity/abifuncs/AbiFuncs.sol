4472c78b6961bacdd78aaacceb2ccfcfa9fb81d3
tonykwok1992 <tonykwok1992@gmail.com>
2024-07-22 20:14:08 +0800

    Make getABI_* calls static (#2079)
    Signed-off-by: tonykwok1992 <tonykwok1992@gmail.com>
    Signed-off-by: Julius Lauterbach <julius.lauterbach@t-online.de>

---
+++ codegen/src/test/resources/solidity/abifuncs/AbiFuncs.sol
@@ -0,0 +1,10 @@
+pragma solidity >= 0.8.7;
+
+contract AbiFuncs {
+
+
+    function hi(uint256 x) public {
+
+    }
+
+}
---
+++ codegen/src/test/resources/solidity/abifuncs/build/java/AbiFuncs.java
@@ -0,0 +1,91 @@
+package org.web3j.unittests.java;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteFunctionCall;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/main/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version none.
+ */
+@SuppressWarnings("rawtypes")
+public class AbiFuncs extends Contract {
+    public static final String BINARY = "Bin file was not provided";
+
+    public static final String FUNC_HI = "hi";
+
+    @Deprecated
+    protected AbiFuncs(String contractAddress, Web3j web3j, Credentials credentials,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected AbiFuncs(String contractAddress, Web3j web3j, Credentials credentials,
+            ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected AbiFuncs(String contractAddress, Web3j web3j, TransactionManager transactionManager,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected AbiFuncs(String contractAddress, Web3j web3j, TransactionManager transactionManager,
+            ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> hi(BigInteger x) {
+        final Function function = new Function(
+                FUNC_HI, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(x)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public static String getABI_hi(BigInteger x) {
+        final Function function = new Function(
+                FUNC_HI, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(x)), 
+                Collections.<TypeReference<?>>emptyList());
+        return org.web3j.abi.FunctionEncoder.encode(function);
+    }
+
+    @Deprecated
+    public static AbiFuncs load(String contractAddress, Web3j web3j, Credentials credentials,
+            BigInteger gasPrice, BigInteger gasLimit) {
+        return new AbiFuncs(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static AbiFuncs load(String contractAddress, Web3j web3j,
+            TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new AbiFuncs(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static AbiFuncs load(String contractAddress, Web3j web3j, Credentials credentials,
+            ContractGasProvider contractGasProvider) {
+        return new AbiFuncs(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static AbiFuncs load(String contractAddress, Web3j web3j,
+            TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new AbiFuncs(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+}
