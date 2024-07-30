04cb2a6a671c6bd0ada40ca3cdd99c3e51507f50
Julius Lauterbach <julius.lauterbach@t-online.de>
2024-07-30 13:37:50 +0200

    fix for new URL (web3j to hyperledger) including one branch fix (master to main)
    Signed-off-by: Julius Lauterbach <julius.lauterbach@t-online.de>

--- abi/src/main/java/org/web3j/abi/datatypes/generated/Int48.java
+++ abi/src/main/java/org/web3j/abi/datatypes/generated/Int48.java
@@ -7,7 +7,7 @@ import org.web3j.abi.datatypes.Int;
  * Auto generated code.
  * <p><strong>Do not modifiy!</strong>
  * <p>Please use org.web3j.codegen.AbiTypesGenerator in the 
- * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ * <a href="https://github.com/hyperledger/web3j/tree/main/codegen">codegen module</a> to update.
  */
 public class Int48 extends Int {
     public static final Int48 DEFAULT = new Int48(BigInteger.ZERO);
