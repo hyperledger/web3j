7898243978ce7eb4da1fa14d633c6f8f5ed9e2bc
Julius Lauterbach <julius.lauterbach@t-online.de>
2024-07-30 14:22:20 +0200

    fix for new URL (web3j to hyperledger) including one branch fix (master to main)
    Signed-off-by: Julius Lauterbach <julius.lauterbach@t-online.de>

--- abi/src/main/java/org/web3j/abi/datatypes/generated/Uint64.java
+++ abi/src/main/java/org/web3j/abi/datatypes/generated/Uint64.java
@@ -7,7 +7,7 @@ import org.web3j.abi.datatypes.Uint;
  * Auto generated code.
  * <p><strong>Do not modifiy!</strong>
  * <p>Please use org.web3j.codegen.AbiTypesGenerator in the 
- * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ * <a href="https://github.com/hyperledger/web3j/tree/main/codegen">codegen module</a> to update.
  */
 public class Uint64 extends Uint {
     public static final Uint64 DEFAULT = new Uint64(BigInteger.ZERO);
